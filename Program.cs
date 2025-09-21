65a7e5e2b432415852ed8750048490ce6798b7ac
arsenijKV <163118921+arsenijKV@users.noreply.github.com>
2025-09-21 22:37:06 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,32 +1,80 @@
-﻿using WebApi.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using WebApi.Data;
 using WebApi.Services;
+using WebApi.Entities;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// add services to DI container
-{
-    var services = builder.Services;
-    services.AddCors();
-    services.AddControllers();
 
-    // configure DI for application services
-    services.AddScoped<IUserService, UserService>();
-}
+builder.Services.AddControllers();
+
+// DbContext 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// PasswordHasher 
+builder.Services.AddScoped<PasswordHasher<User>>();
+
+
+builder.Services.AddScoped<IUserService, UserService>();
+
+// JWT
+var jwtSection = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSection["Key"]!);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false; 
+    options.SaveToken = true;
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSection["Issuer"],
+        ValidAudience = jwtSection["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddAuthorization();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-// configure HTTP request pipeline
+// middleware
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+using (var scope = app.Services.CreateScope())
 {
-    // global cors policy
-    app.UseCors(x => x
-        .AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader());
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate(); 
 
-    // custom basic auth middleware
-    //app.UseMiddleware<BasicAuthMiddleware>();
-
-    app.MapControllers();
+    var hasher = scope.ServiceProvider.GetRequiredService<PasswordHasher<User>>();
+    if (!db.Users.Any())
+    {
+        var admin = new User { Username = "admin", FirstName = "Admin", LastName = "User" };
+        admin.PasswordHash = hasher.HashPassword(admin, "admin123"); // пароль admin123
+        db.Users.Add(admin);
+        db.SaveChanges();
+    }
 }
 
 app.Run("http://localhost:5000");
