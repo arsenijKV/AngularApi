ed79a9c93cb8bfe75adbb0df95303d4b5bdea4e2
arsenijKV <163118921+arsenijKV@users.noreply.github.com>
2025-09-21 22:46:10 +0300

    Update user.service.ts

--- ClientApp/src/app/services/user.service.ts
+++ ClientApp/src/app/services/user.service.ts
@@ -1,32 +1,52 @@
+// src/app/services/auth.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+
+interface AuthResponse {
+  token: string;
+  id?: number;
+  username?: string;
+  // другие поля, которые присылает сервер
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:5000'; 
-
+  private base = 'http://localhost:5000';
   constructor(private http: HttpClient) { }
 
-  login(username: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/authenticate`, { username, password });
+  login(username: string, password: string): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.base}/auth/authenticate`, { username, password })
+      .pipe(map(res => {
+        if (res && (res as any).token) {
+          this.saveToken((res as any).token);
+        }
+        return res;
+      }));
   }
 
-  register(firstName: string, lastName: string, username: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/register`, { firstName, lastName, username, password });
+  register(firstName: string, lastName: string, username: string, password: string) {
+    return this.http.post(`${this.base}/auth/register`, { firstName, lastName, username, password });
   }
 
-  saveUser(user: any) {
-    localStorage.setItem('user', JSON.stringify(user));
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
   }
 
-  getUser() {
-    return JSON.parse(localStorage.getItem('user') || '{}');
+  getToken(): string | null {
+    return localStorage.getItem('token');
   }
 
   logout() {
+    localStorage.removeItem('token');
     localStorage.removeItem('user');
+  }
+
+  // ещё полезные методы
+  isLogged(): boolean {
+    return !!this.getToken();
   }
 }
