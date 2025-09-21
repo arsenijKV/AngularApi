cb43306e8613a75360e5038a10ec72160cdeb1ff
arsenijKV <163118921+arsenijKV@users.noreply.github.com>
2025-09-21 22:48:46 +0300

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+
+namespace WebApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
---
+++ Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿// Data/DesignTimeDbContextFactory.cs
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace WebApi.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var config = builder.Build();
+            var conn = config.GetConnectionString("DefaultConnection")
+                       ?? "Server=localhost;Database=WebApiDb;Trusted_Connection=True;";
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(conn);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
